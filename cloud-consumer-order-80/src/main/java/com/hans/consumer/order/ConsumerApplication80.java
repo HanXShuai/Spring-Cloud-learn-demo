16e742e4be45ad049d57f42aa2672d5f33222dd7
hans <hanhshuai@163.com>
2021-12-12 21:47:17 +0800

    注册进 eureka 集群

--- cloud-consumer-order-80/src/main/java/com/hans/consumer/order/ConsumerApplication80.java
+++ cloud-consumer-order-80/src/main/java/com/hans/consumer/order/ConsumerApplication80.java
@@ -2,12 +2,14 @@ package com.hans.consumer.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: hans
  * @Date: 2021/12/12 13:25
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsumerApplication80 {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication80.class, args);
