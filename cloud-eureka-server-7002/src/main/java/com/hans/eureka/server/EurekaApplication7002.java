2cca5464e8725e2fa032db295a819ef7df4ceefe
hans <hanhshuai@163.com>
2021-12-12 21:45:18 +0800

    eureka 集群配置

---
+++ cloud-eureka-server-7002/src/main/java/com/hans/eureka/server/EurekaApplication7002.java
@@ -0,0 +1,17 @@
+package com.hans.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: hans
+ * @Date: 2021/12/12 21:14
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication7002.class, args);
+    }
+}
