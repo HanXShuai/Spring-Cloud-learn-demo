39dbc123cbba49d68800db758978d4c6dfd56646
hans <hanhshuai@163.com>
2022-01-06 21:19:54 +0800

    gateway

---
+++ cloud-gateway-gateway-9527/src/main/java/com/hans/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.hans.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: hans
+ * @Date: 2022/1/6 19:42
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
